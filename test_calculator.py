96a16a2ac337dec0638874e972e4c9123f1a12fc
lau967 <201292700+elau967@users.noreply.github.com>
2025-11-17 23:28:17 -0500

    partner1 changes

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
+    def test_multiply(self): # 3 assertions
     #     fill in code
+        self.assertEqual(mul(5, 4), 20)
+        self.assertEqual(mul(0.25, 10), 25)
+        self.assertEqual(mul(3, 0), 0)
 
-    # def test_divide(self): # 3 assertions
+    def test_divide(self): # 3 assertions
     #     fill in code
     # ##########################
+        self.assertEqual(div(5, 1), 5)
+        self.assertEqual(div(2, 10), 0.2)
+        self.assertEqual(div(3, 3), 1)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,21 +40,29 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
     #     fill in code
+        with self.assertRaises(ValueError):
+            logarithm(5, -5)
 
-    # def test_hypotenuse(self): # 3 assertions
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(-3, -4), 5)
+        self.assertEqual(hypotenuse(6, 8), 10)
     #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
+    def test_sqrt(self): # 3 assertions
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #    square_root(NUM)
     #     # Test basic function
     #     fill in code
+
+        with self.assertRaises(ValueError):
+            square_root(-2)
     ##########################
 
 # Do not touch this
